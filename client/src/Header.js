6fe06d4ae0c20fec1ccf5da36d00157a124b365c
Joshua Blasbalg <joshua.blasbalg@gmail.com>
2018-09-18 15:40:48 -0400

    styling3

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-md  navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-md  navbar-dark bg-dark sticky-top">
         <a className="navbar-brand" href="#">
           <img
             src="/logo.png"
--- client/src/Home.js
+++ client/src/Home.js
@@ -27,10 +27,10 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="bg-secondary">
+      <div className="bg-secondary" style={{ height: 800 + "px" }}>
         <Header />
         <Footer />
-        <div className="container card-columns ml-auto mr-auto mt-5 row d-flex justify-content-between ">
+        <div className="container card-columns ml-auto mr-auto mt-3 row d-flex justify-content-between ">
           <div className="card col-lg-5 bg-dark text-center order-lg-1  ">
             <img
               class="card-img-top rounded-circle mt-3"
