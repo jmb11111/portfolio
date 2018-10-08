131911780f39924faed72423d28053c6c9acde45
Joshua Blasbalg <joshua.blasbalg@gmail.com>
2018-10-08 16:04:15 -0400

    breakpoint footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <footer class="font-small fixed-bottom cardBackground footer-copyright text-light text-center py-3">
+      <footer class="font-small mastfoot cardBackground footer-copyright text-light text-center py-3">
         © 2018 Copyright Joshua Blasbalg
       </footer>
     );
